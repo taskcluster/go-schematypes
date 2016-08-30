d1981c02202453604dc830702c9160f98b739838
Jonas Finnemann Jensen <jopsen@gmail.com>
2016-08-30 11:27:55 -0700

    Added Map from string to values

---
+++ map_test.go
@@ -0,0 +1,102 @@
+package schematypes
+
+import "testing"
+
+func TestMapInteger(t *testing.T) {
+	var smap map[string]int
+	testCase{
+		Schema: Map{
+			MetaData: MetaData{
+				Title:       "my-title-1",
+				Description: "my-description-1",
+			},
+			Values: Integer{
+				MetaData: MetaData{
+					Title:       "my-title-2",
+					Description: "my-description-2",
+				},
+				Minimum: -240,
+				Maximum: 240,
+			},
+		},
+		Match: `{
+      "type": "object",
+      "title": "my-title-1",
+      "description": "my-description-1",
+      "additionalProperties": {
+        "type": "integer",
+        "title": "my-title-2",
+        "description": "my-description-2",
+        "minimum": -240,
+        "maximum": 240
+      }
+    }`,
+		Valid: []string{
+			"{}",
+			"{\"test\":4, \"test2\":67, \"test3\":76}",
+			"{\"test\":4, \"test2\":67, \"test3\":76, \"hello world\": 67}",
+		},
+		Invalid: []string{
+			"56", "34.3", "\"dfsdf\"",
+			"[4,67,7.66]", "[4,67,500]",
+			"{\"test\":4, \"test2\":67, \"test3\":500}",
+		},
+		TypeMatch: []interface{}{
+			&smap,
+		},
+		TypeMismatch: []interface{}{
+			&[]int32{},
+			&[]int{},
+			&[]int8{},
+			&[]struct{}{},
+			&[]string{},
+		},
+	}.Test(t)
+}
+
+func TestMapString(t *testing.T) {
+	var smap map[string]string
+	testCase{
+		Schema: Map{
+			MetaData: MetaData{
+				Title:       "my-title-1",
+				Description: "my-description-1",
+			},
+			Values: String{
+				MetaData: MetaData{
+					Title:       "my-title-2",
+					Description: "my-description-2",
+				},
+			},
+		},
+		Match: `{
+      "type": "object",
+      "title": "my-title-1",
+      "description": "my-description-1",
+      "additionalProperties": {
+        "type": "string",
+        "title": "my-title-2",
+        "description": "my-description-2"
+      }
+    }`,
+		Valid: []string{
+			"{}",
+			"{\"test\":\"4 \", \"test2 lala \":\"67\", \"test3\":\"hello world\"}",
+		},
+		Invalid: []string{
+			"56", "34.3", "\"dfsdf\"",
+			"[4,67,7.66]", "[4,67,500]",
+			"{\"test\":4, \"test2\":67, \"test3\":500}",
+		},
+		TypeMatch: []interface{}{
+			&smap,
+		},
+		TypeMismatch: []interface{}{
+			&[]int32{},
+			&[]int{},
+			&[]int8{},
+			&[]struct{}{},
+			&[]string{},
+		},
+	}.Test(t)
+}
