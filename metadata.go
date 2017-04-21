e27df4e7052116a38a345916d8894007449006ad
Jonas Finnemann Jensen <jopsen@gmail.com>
2017-04-20 18:25:37 -0700

    Removed MetaData struct

--- metadata.go
+++ metadata.go
@@ -1,22 +1,5 @@
 package schematypes
 
-// MetaData contains meta-data properties common to all schema types.
-type MetaData struct {
-	Title       string
-	Description string
-}
-
-func (s *MetaData) schema() map[string]interface{} {
-	m := make(map[string]interface{})
-	if s.Title != "" {
-		m["title"] = s.Title
-	}
-	if s.Description != "" {
-		m["description"] = s.Description
-	}
-	return m
-}
-
 func makeMetaData(title, description string) map[string]interface{} {
 	m := make(map[string]interface{})
 	if title != "" {
